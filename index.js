b5af5e7397d6a7c9347fba3869b40677dbdd87f1
arrocito82 <54961047+Arrocito82@users.noreply.github.com>
2020-08-08 14:07:12 -0600

    changing to the latest version of the project
    This way i'll work better

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 //getting all the buttons
 var buttons = document.getElementsByClassName("drum");
 
-var crash = new Audio("sounds/crash.mp3");
-var kickBass = new Audio("sounds/kick-bass.mp3");
-var snare = new Audio("sounds/snare.mp3");
-var tom1 = new Audio("sounds/tom-1.mp3");
-var tom2 = new Audio("sounds/tom-2.mp3");
-var tom3 = new Audio("sounds/tom-3.mp3");
-var tom4 = new Audio("sounds/tom-4.mp3");
-
-
 //function to display the alert that says i got clicked
 // function displayAlert() {
 //     crash.play();
@@ -17,65 +8,51 @@ var tom4 = new Audio("sounds/tom-4.mp3");
 
 
 for (let index = 0; index < buttons.length; index++) {
+
     buttons[index].addEventListener("click", function() {
         var key = this.innerHTML;
-
-        switch (key) {
-            case "w":
-                tom1.play();
-                break;
-            case "a":
-                crash.play();
-                break;
-            case "s":
-                kickBass.play();
-                break;
-            case "d":
-                snare.play();
-                break;
-            case "j":
-                tom2.play();
-                break;
-            case "k":
-                tom3.play();
-                break;
-            case "l":
-                tom4.play();
-                break;
-            default:
-                break;
-        }
-
+        makeSound(key);
     });
 
     // thispartshouldbethekeypressonthekeyboardcaptured.
     document.addEventListener("keydown", function(event) {
 
-        switch (event.key) {
-            case "w":
-                tom1.play();
-                break;
-            case "a":
-                crash.play();
-                break;
-            case "s":
-                kickBass.play();
-                break;
-            case "d":
-                snare.play();
-                break;
-            case "j":
-                tom2.play();
-                break;
-            case "k":
-                tom3.play();
-                break;
-            case "l":
-                tom4.play();
-                break;
-            default:
-                break;
-        }
-
+        makeSound(event.key);
     });
+}
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "s":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+        case "d":
+
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        default:
+            break;
+    }
 }
